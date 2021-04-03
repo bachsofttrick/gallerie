83e5e70d889204c6827fbb61db7a503119df5cd4
bach_oneicc <bach.phan@oneicc.com>
2021-04-04 00:26:15 +0700

    try setting routes for express

---
+++ src/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const Client = require('ftp');
+const serveIndex = require('serve-index');
+const app = express();
+const port = 3000;
+
+const ftpInfo = {
+  user: 'film',
+  password: ''
+};
+let listFtp = {};
+const dir = 'd:/camera';
+const dirFilm = 'd:/film';
+
+// Ftp client
+const ftp = new Client();
+ftp.on('ready', () => {
+  ftp.list('/', (err, list) => {
+    if (err) throw err;
+    listFtp = list;
+  });
+});
+
+// The express.static serves the file contents
+// The serveIndex is this module serving the directory
+app.use('/', express.static(dir), serveIndex(dir));
+
+// Send file to display on browser
+// Download tags Content-Disposition: attachment to Save As
+app.get('/cam', (req, res) => {
+  const fileTest = `${dir}/20210101_000149.jpg`;
+  res.sendFile(fileTest);
+});
+
+app.get('/pjmasks', (req, res) => {
+  const fileTest = `${dirFilm}/pj masks.mp4`;
+  res.sendFile(fileTest);
+});
+
+// Starting server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,5 +14,12 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public check = 0;
+
+  public mounted(): void {
+    return;
+  }
+  
+}
 </script>
