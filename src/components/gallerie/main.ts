38b8bd7a26b99f490ce4769e5912217b60fce15b
bachsofttrick <xuanbach1307@gmail.com>
2021-10-18 23:15:11 +0700

    show images from apis

--- src/components/gallerie/main.ts
+++ src/components/gallerie/main.ts
@@ -1,23 +1,29 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
 
 @Component
 export default class Gallerie extends Vue {
-    public picLinks = ['http://localhost:3000/html/picture/WIN_20210225_11_08_47_Pro.jpg', 'http://localhost:3000/html/thumbnail/WIN_20210115_10_38_31_Pro.jpg'];
-    private times = 2;
+    private picLinks: string[] = [];
+    private count = 0;
 
     public mounted(): void {
-        return;
+        // Lazy load images at the bottom
+        window.onscroll = () => {
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                this.count += 10;
+                this.getPictures(this.count);
+            }
+        };
+        this.getPictures(this.count);
     }
 
-    public multiplySrc(times: number): void {
-        for (let i = 0; i < times; i++) {
-            this.picLinks = this.picLinks.concat(this.picLinks);
-        }
-    }
-    
-    public deleteHalf(): void {
-        const halfNumber = Math.round(this.picLinks.length / 2);
-        if (halfNumber > 1) this.picLinks = this.picLinks.slice(0, halfNumber);
+    public getPictures(count: number): void {
+        axios.get('http://localhost:3000/testcam', {params: {skip: this.count}}).then((value) => {
+            for (let i = 0; i < value.data.data.length; i++) {
+                value.data.data[i] = 'http://localhost:3000/resi/' + value.data.data[i];
+                this.picLinks.push(value.data.data[i]);
+            }
+        });
     }
 
     public goTop(): void {
--- src/components/gallerie/main.vue
+++ src/components/gallerie/main.vue
@@ -2,11 +2,6 @@
   <div>
     <div class="thumbnail-list">
       <img v-for="(source, id) in picLinks" :key="id" :src="source">
-      <div class="multiply-time">
-        <input v-model="times" />
-        <button @click="multiplySrc(times)">Multiply</button>
-        <button @click="deleteHalf">Delete</button>
-      </div>
     </div>
     <div class="to-top">
       <button @click="goTop">Top</button>
